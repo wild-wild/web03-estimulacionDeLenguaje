de8205dfb7fa90ee8fb56438f4e13415214aa81c
wild <wild.addsystem@gmail.com>
2024-03-09 15:15:48 -0400

    act
    act

---
+++ public/js/ajustes.js
@@ -0,0 +1,7 @@
+const nombrePack = document.getElementById('nombrePack');
+// precio
+const precio = document.getElementById('precio');
+nombrePack.innerHTML = 'PACK DE ESTIMULACIÓN DE LENGUAJE (2-7 AÑOS)';
+precio.innerHTML = 'Precio: 10 USD';
+
+
